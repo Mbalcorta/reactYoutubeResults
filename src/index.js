fe9626467536715df9053b66046f896afff7f160
Mateo Balcorta <misterrattail@gmail.com>
2018-04-21 21:42:11 -0700

    Add file to ignore.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from 'youtube-api-search';
+import SearchBar from "./components/search_bar";
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+const API_KEY = "AIzaSyDUbHTORNvzPSmHoDB3UQi3b4N-s2X4J_A";
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component. This component shoudl produce some html
+class App extends Component {
+    constructor(props){
+        super(props)
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        return YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        })
+    }
+    
+    render () {
+        return (
+            <div>
+                <SearchBar onSearchTermChange={ (term) => this.videoSearch(term)} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect = { selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos }
+                />
+            </div>
+            );
+    }
+};
+
+//take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
